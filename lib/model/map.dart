424320baf4da13548905d4c98f192c20d4ce6a7b
hanmango-o <hanmango.o@gmail.com>
2023-06-04 14:59:29 +0900

    add : create map model

--- lib/model/map.dart
+++ lib/model/map.dart
@@ -1,1 +1,29 @@
+import 'package:wumpus_world/model/agent.dart';
 
+import '../core/data/enums.dart';
+
+class Map {
+  List<List<Area>> areas = [
+    [Area(danger: Danger.safe), Area(), Area(), Area()],
+    [Area(), Area(), Area(), Area()],
+    [Area(), Area(), Area(), Area()],
+    [Area(), Area(), Area(), Area()],
+  ];
+
+  // Area point(int x, int y) {
+  //   return _areas[3 - x][y];
+  // }
+
+  Agent agent = Agent();
+
+  void deathEvent() {
+    agent = Agent();
+  }
+}
+
+class Area {
+  List<State> state = [];
+  Danger danger;
+
+  Area({this.danger = Danger.unKnown});
+}
